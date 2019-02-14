a7090cf26df2de2db13c57928a4d26a1c14d8fdf
nedmorgan <nmorgan1987@gmail.com>
2019-02-14 14:39:27 -0500

    added function to display none if no player 2

--- js/main.js
+++ js/main.js
@@ -13,12 +13,16 @@ let startButton = document.getElementById('startButton')
 function input1() {
   player1.name = prompt(`Player 1, please input your name.`)
   document.getElementById('player1').innerHTML = player1.name
-  
+
 }
 
 function input2() {
   player2.name = prompt(`Player 2, please input your name.`)
-  document.getElementById('player2').innerHTML = player2.name
+  if (player2.name === null) {
+    document.querySelector('.player2-score').setAttribute('style', 'display:none')
+  } else {
+    document.getElementById('player2').innerHTML = player2.name
+  }
 }
 
 startButton.addEventListener('click', function () {
