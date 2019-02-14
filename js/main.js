ae2a37b11d3fcc5e1cedf75dc4b461096f6c4ce2
nedmorgan <nmorgan1987@gmail.com>
2019-02-14 14:26:23 -0500

    added input functionality to add player to constructor

---
+++ js/main.js
@@ -0,0 +1,27 @@
+class Player {
+  constructor(name, score) {
+    this.name = name
+    this.score = score
+    this.colorSeq = []
+  }
+}
+
+let player1 = new Player()
+let player2 = new Player()
+let startButton = document.getElementById('startButton')
+
+function input1() {
+  player1.name = prompt(`Player 1, please input your name.`)
+  document.getElementById('player1').innerHTML = player1.name
+  
+}
+
+function input2() {
+  player2.name = prompt(`Player 2, please input your name.`)
+  document.getElementById('player2').innerHTML = player2.name
+}
+
+startButton.addEventListener('click', function () {
+  input1()
+  input2()
+})
