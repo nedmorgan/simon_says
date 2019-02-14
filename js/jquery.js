8ed3fd4284b62c45a34a5d26eea52757c6fbe58f
nedmorgan <nmorgan1987@gmail.com>
2019-02-14 15:15:28 -0500

    added audio files to gameplay

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-  
+  console.log(`jQuery is ready`)
 })
--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@ class Player {
   }
 }
 
+let gameplay = {
+  level: 0,
+  colors:['#yellow', '#red', '#green', '#blue'],
+  players: [],
+  sounds: {
+    green: new Audio('media/sfx_sounds_Blip1.wav'),
+    red: new Audio('media/sfx_sounds_Blip4.wav'),
+    yellow: new Audio('media/sfx_sounds_Blip8.wav'),
+    blue: new Audio('media/sfx_sounds_Blip10.wav')
+  }
+}
+
+document.getElementById('resetButton').addEventListener('click', function() {
+  console.log(gameplay.sounds.green)
+  gameplay.sounds.blue.play();
+})
+
 let player1 = new Player()
 let player2 = new Player()
 let startButton = document.getElementById('startButton')
