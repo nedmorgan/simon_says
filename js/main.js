1d72b88bb5dd28336cc20699ef5cb95f033899ff
nedmorgan <nmorgan1987@gmail.com>
2019-02-16 15:07:06 -0500

    game now works with single click. need to reset array each time a new color is called and have that get checked

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // player constructor
 class Player {
-  constructor (name) {
+  constructor(name) {
     this.name = name
     this.score = 0
     this.colorSeq = []
@@ -32,8 +32,8 @@ let defaultGame = {
 }
 
 // new player variables
-let player1 = new Player(name)
-let player2 = new Player(name)
+let player1 = new Player()
+let player2 = new Player()
 
 // game score variable
 let value = 1
@@ -49,38 +49,36 @@ const yellow = document.getElementById('yellow')
 const blue = document.getElementById('blue')
 
 // set current player function
-function curPlayer (name) {
+function curPlayer(name) {
   game.currentPlayer = name
   document.getElementById('player-banner').innerHTML = name
 }
 
 // function to keep score
-function score (num) {
+function score(num) {
   game.score += num
 }
 
 // input for player 1
-function input1 () {
-  let playName1 = ''
-  playName1 = prompt(`Player 1, please input your name.`)
-  let player1 = new Player(playName1)
+function input1() {
+  player1.name = prompt('Player 1, please input your name.')
   document.getElementById('player-one').innerHTML = player1.name
   game.players.push(player1.name)
   curPlayer(player1.name)
 }
 
 // input for player 2 and if not then take away player 2
-function input2 () {
+function input2() {
   player2.name = prompt(`Player 2, please input your name.`)
   document.getElementById('player-two').innerHTML = player2.name
   game.players.push(player2.name)
   if (player2.name === '') {
     document.querySelector('.player2-score').setAttribute('style', 'display:none')
-  } 
+  }
 }
 
 // function to check if arrays match
-function checkArray (arr1, arr2) {
+function checkArray(arr1, arr2) {
   arr1.forEach((e1) => arr2.forEach((e2) => {
     if (e1 !== e2) {
       game.track = false
@@ -91,7 +89,7 @@ function checkArray (arr1, arr2) {
 }
 
 // function to decrease game interval
-function decreaseTimer () {
+function decreaseTimer() {
   if (game.interval > 0) {
     game.interval -= 150
     console.log(game.interval)
@@ -101,7 +99,7 @@ function decreaseTimer () {
 }
 
 // the color will show and the sound will play when a color div is clicked
-function greenClick () {
+function greenClick() {
   let index = 1
   green.classList.add('green-blur')
   game.sounds.green.play()
@@ -125,23 +123,24 @@ function greenClick () {
       game.sequence = []
       game.score = 0
     }
-  } else if (player2.name !== null && player2.name === game.currentPlayer) {
+  } else if (player2.name === game.currentPlayer) {
     player2.colorSeq.push(index)
-    checkArray(player1.colorSeq, game.sequence)
+    checkArray(player2.colorSeq, game.sequence)
     if (game.track === true) {
       score(value)
       player2.score = game.score
       document.getElementById('two-score').innerHTML = player2.score
     } else {
+      alert(`Bad Choice. You have lost!`)
       if (player2.score > player1.score) {
         document.getElementById('best-score').innerHTML = player2.score
-        alert(`Bad Choice. You have lost!`)
+        alert(`${player2.name} you have the high score`)
       }
     }
   }
 }
 
-function redClick () {
+function redClick() {
   let index = 2
   red.classList.add('red-blur')
   game.sounds.red.play();
@@ -165,7 +164,7 @@ function redClick () {
       game.sequence = []
       game.score = 0
     }
-  } else if (player2.name !== null && player2.name === game.currentPlayer) {
+  } else if (player2.name === game.currentPlayer) {
     player2.colorSeq.push(index)
     checkArray(player1.colorSeq, game.sequence)
     if (game.track === true) {
@@ -173,15 +172,16 @@ function redClick () {
       player2.score = game.score
       document.getElementById('two-score').innerHTML = player2.score
     } else {
+      alert(`Bad Choice. You have lost!`)
       if (player2.score > player1.score) {
         document.getElementById('best-score').innerHTML = player2.score
-        alert(`Bad Choice. You have lost!`)
+        alert(`${player2.name} you have the high score`)
       }
     }
   }
 }
 
-function yellowClick () {
+function yellowClick() {
   let index = 3
   yellow.classList.add('yellow-blur')
   game.sounds.yellow.play();
@@ -205,23 +205,24 @@ function yellowClick () {
       game.sequence = []
       game.score = 0
     }
-  } else if (player2.name !== null && player2.name === game.currentPlayer) {
+  } else if (player2.name === game.currentPlayer) {
     player2.colorSeq.push(index)
-    checkArray(player1.colorSeq, game.sequence)
+    checkArray(player2.colorSeq, game.sequence)
     if (game.track === true) {
       score(value)
       player2.score = game.score
       document.getElementById('two-score').innerHTML = player2.score
     } else {
+      alert(`Bad Choice. You have lost!`)
       if (player2.score > player1.score) {
         document.getElementById('best-score').innerHTML = player2.score
-        alert(`Bad Choice. You have lost!`)
+        alert(`${player2.name} you have the high score`)
       }
     }
   }
 }
 
-function blueClick () {
+function blueClick() {
   let index = 4
   blue.classList.add('blue-blur')
   game.sounds.blue.play();
@@ -245,57 +246,58 @@ function blueClick () {
       game.sequence = []
       game.score = 0
     }
-  } else if (player2.name !== null && player2.name === game.currentPlayer) {
+  } else if (player2.name === game.currentPlayer) {
     player2.colorSeq.push(index)
-    checkArray(player1.colorSeq, game.sequence)
+    checkArray(player2.colorSeq, game.sequence)
     if (game.track === true) {
       score(value)
       player2.score = game.score
       document.getElementById('two-score').innerHTML = player2.score
     } else {
+      alert(`Bad Choice. You have lost!`)
       if (player2.score > player1.score) {
         document.getElementById('best-score').innerHTML = player2.score
-        alert(`Bad Choice. You have lost!`)
+        alert(`${player2.name} you have the high score`)
       }
     }
   }
 }
 
 // function to automatically have the color blur and sound play
-function greenShow () {
+function greenShow() {
   let numVal = 1
   green.classList.add('green-blur')
-  game.sounds.green.play();
+  game.sounds.green.play()
   setTimeout(function () {
     green.classList.remove('green-blur')
   }, game.interval)
   game.sequence.push(numVal)
 }
 
-function redShow () {
+function redShow() {
   let numVal = 2
   red.classList.add('red-blur')
-  game.sounds.red.play();
+  game.sounds.red.play()
   setTimeout(function () {
     red.classList.remove('red-blur')
   }, game.interval)
   game.sequence.push(numVal)
 }
 
-function yellowShow () {
+function yellowShow() {
   let numVal = 3
   yellow.classList.add('yellow-blur')
-  game.sounds.yellow.play();
+  game.sounds.yellow.play()
   setTimeout(function () {
     yellow.classList.remove('yellow-blur')
   }, game.interval)
   game.sequence.push(numVal)
 }
 
-function blueShow () {
+function blueShow() {
   let numVal = 4
   blue.classList.add('blue-blur')
-  game.sounds.blue.play();
+  game.sounds.blue.play()
   setTimeout(function () {
     blue.classList.remove('blue-blur')
   }, game.interval)
@@ -303,7 +305,7 @@ function blueShow () {
 }
 
 // function to math random through the game array of numbers
-function random () {
+function random() {
   let randomNum = Math.ceil(Math.random() * 4)
   if (randomNum === 1) {
     greenShow()
@@ -318,12 +320,12 @@ function random () {
 }
 
 // async function to activate game
-async function gameplay () {
+async function gameplay() {
 
 }
 
 // function to reset the game to start from scratch
-function reset () {
+function reset() {
   game.interval = defaultGame.interval
   document.getElementById('player-one').innerHTML = defaultGame.player1
   document.getElementById('player-two').innerHTML = defaultGame.player2
